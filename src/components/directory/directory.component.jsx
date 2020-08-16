1262aa6f0a8be3ab9bdd68279f24786a90ccaec1
Steven Abaco <steve@wizardrywd.com>
2020-08-15 23:30:25 -0500

    Update menu items to navigate withRouter HOC

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -11,32 +11,37 @@ class Directory extends React.Component {
       sections: [
         {
           title: 'hats',
-          imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+          imageUrl:
+            'https://images.unsplash.com/photo-1513105737059-ff0cf0580ae6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
           id: 1,
-          linkUrl: 'shop/hats',
+          linkUrl: 'hats',
         },
         {
           title: 'jackets',
-          imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+          imageUrl:
+            'https://images.pexels.com/photos/16170/pexels-photo.jpg?auto=compress&cs=tinysrgb&h=750&w=1260',
           id: 2,
           linkUrl: 'shop/jackets',
         },
         {
           title: 'sneakers',
-          imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+          imageUrl:
+            'https://images.unsplash.com/photo-1527090526205-beaac8dc3c62?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80',
           id: 3,
           linkUrl: 'shop/sneakers',
         },
         {
           title: 'womens',
-          imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+          imageUrl:
+            'https://images.pexels.com/photos/3781570/pexels-photo-3781570.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
           size: 'large',
           id: 4,
           linkUrl: 'shop/womens',
         },
         {
           title: 'mens',
-          imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+          imageUrl:
+            'https://images.pexels.com/photos/842811/pexels-photo-842811.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260',
           size: 'large',
           id: 5,
           linkUrl: 'shop/mens',
@@ -48,8 +53,8 @@ class Directory extends React.Component {
   render() {
     return (
       <div className='directory-menu'>
-        {this.state.sections.map(({ title, imageUrl, id, size }) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+        {this.state.sections.map(({ id, ...otherSectionProps }) => (
+          <MenuItem key={id} {...otherSectionProps} />
         ))}
       </div>
     );
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -6,7 +6,7 @@ import Directory from '../../components/directory/directory.component';
 
 const HomePage = () => (
   <div className='homepage'>
-    <Directory />
+    <Directory/>
   </div>
 );
 
